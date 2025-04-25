e503483e519744886c9b14b2402c9056c4333fb0
checolato <chenchenhsieh1210@gmail.com>
2025-04-25 15:58:54 -0400

    try2

--- sketch.js
+++ sketch.js
@@ -1,30 +1,27 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-}
-
 let baseImg, rainbowImg;
 
 function preload() {
-  baseImg    = loadImage('./asset/schoolgirl.png');
-  rainbowImg = loadImage('./asset/rainbow.png');
+  baseImg    = loadImage('asset/schoolgirl.png');
+  rainbowImg = loadImage('asset/rainbow.png');
 }
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
+  const cnv = createCanvas(windowWidth, windowHeight);
+  cnv.position(0, 0);
   imageMode(CENTER);
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function draw() {
+  // do NOT clear the background – we want a “brush” trail
   if (mouseIsPressed) {
     image(rainbowImg, mouseX, mouseY, 100, 100);
   } else {
-    tint(255, 127);                      
+    tint(255, 127);            // make the school-girl semi-transparent
     image(baseImg, mouseX, mouseY, 100, 100);
-    noTint();          
+    noTint();
   }
 }
